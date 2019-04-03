6aa47809a2b4bf2a7270655272c420db704a476d
Romil Parhwal <romilparhwal007@gmail.com>
2019-04-02 23:27:24 -0400

    Lazy OS Bringup
    Change-Id: I77c5f0ceabdf0f1ad957d721d89eebc0114ac86c

--- scripts/microfactory.bash
+++ scripts/microfactory.bash
@@ -59,7 +59,7 @@ function soong_build_go
     BUILDDIR=$(getoutdir) \
       SRCDIR=${TOP} \
       BLUEPRINTDIR=${TOP}/build/blueprint \
-      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path lineage/soong=${TOP}/vendor/lineage/build/soong" \
+      EXTRA_ARGS="-pkg-path android/soong=${TOP}/build/soong -pkg-path lazy/soong=${TOP}/vendor/lazy/build/soong" \
       build_go $@
 }
 
